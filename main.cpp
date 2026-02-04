aaf00aeecc4fa75a8a3cb704ae7a0568b54a150c
landinjm <landinjm@umich.edu>
2026-02-03 22:48:24 -0500

    eigen dependency

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+#include <Eigen/Dense>
+#include <iostream>
+
 int
 main(int argc, char** argv)
 {
+  // Create a matrix and print it
+  Eigen::MatrixXd m(2, 2);
+  m(0, 0) = 3;
+  m(1, 0) = 2.5;
+  m(0, 1) = -1;
+  m(1, 1) = m(1, 0) + m(0, 1);
+  std::cout << m << std::endl;
+
   return 0;
 }
