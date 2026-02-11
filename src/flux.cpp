483d1df490086881802fd723eb1a91632a099885
tjmellema <115052434+tjmellema@users.noreply.github.com>
2026-02-11 00:49:29 -0500

    Update flux.cpp with Cell_flux functionality

--- src/flux.cpp
+++ src/flux.cpp
@@ -18,19 +18,6 @@ Flux_HLLE(Tensor<1,4,double> UL,
     double uR = UR[1] / rhoR;
     double vR = UR[2] / rhoR;
 
-    //define un, q, p, rH, c
-    double unL = uL * n[0] + vL*n[1];
-    double qL = sqrt(std::pow(UL[1],2) + std::pow(UL[2],2)) / rhoL;
-    double pL = (gamma - 1) * (UL[3] - 0.5*rhoL*std::pow(qL,2));
-    double rHL = UL[3] + pL;
-    double cL = sqrt(gamma*pL/rhoL);
-
-    double unR = uR * n[0] + vR*n[1];
-    double qR = sqrt(std::pow(UR[1],2) + std::pow(UR[2],2)) / rhoR;
-    double pR = (gamma - 1) * (UR[3] - 0.5*rhoR*std::pow(qR,2));
-    double rHR = UR[3] + pR;
-    double cR = sqrt(gamma*pR/rhoR);
-
     //calculate wave speeds
     double sLmin = min(0, uL - cL);
     double sRmin = min(0, uR - cR);
@@ -44,25 +31,42 @@ Flux_HLLE(Tensor<1,4,double> UL,
     double sLRmin = min(sLmin, sRmin);
     double sLRmax = max(sLmax, sRmax);
 
-    //calculate he left and right fluxes
-    Tensor<1,4,double> FL;
-    Tensor<1,4,double> FR;
+    //calculate the left and right fluxes
+    Tensor<1,4,double> FL = Cell_Flux(UL, n, gamma);
+    Tensor<1,4,double> FR = Cell_Flux(UR, n, gamma);;
     Tensor<1,4,double> F;
 
-    FL[0] = rhoL * unL;
-    FL[1] = UL[1] * unL + pL * n[0];
-    FL[2] = UL[2] * unL + pL * n[1];
-    FL[3] = rHL*unL;
-
-    FR[0] = rhoR * unR;
-    FR[1] = UR[1] * unR + pR * n[0];
-    FR[2] = UR[2] * unR + pR * n[1];
-    FR[3] = rHR*unR;
 
     //TODO include tensor operators
     F = 0.5 * (FL + FR) - 0.5 * (sLRmax + sLRmin)/(slRmax - sLRmin)*(FR - FL) + sLRmax*sLRmin/(sLRmax - sLRmin)*(UR-UL);
 
-    //return the numerical flux and wave speed 
-    return {F, smag};
+    //return the numerical flux and wave speed
+    std::pair<Tensor<1,4,double>, double> exports = {F, smag};
+    return exports;
+    
+}//end Flux_HLLE
 
-}
+Tensor<1,4,double> Cell_Flux(Tensor<1,4,double> U, Tensor<1,2,double> n) {
+    Tensor<1,4,double> F;
+    double gamma = Parameters<double>::gamma;
+
+    //unwrap the state
+    double rho = U[0];
+    double u = U[1] / rho;
+    double v = U[2] / rho;
+
+    //define un, q, p, rH, c
+    double un = u * n[0] + v*n[1];
+    double q = sqrt(std::pow(U[1],2) + std::pow(U[2],2)) / rho;
+    double p = (gamma - 1) * (U[3] - 0.5*rho*std::pow(q,2));
+    double rH = U[3] + p;
+    double c = sqrt(gamma*p/rho);
+
+    //compute the flux
+    F[0] = rho * un;
+    F[1] = U[1] * un + p * n[0];
+    F[2] = U[2] * un + p * n[1];
+    F[3] = rH*un;
+
+    return F;
+}//end Cell_Flux
