cb6056d5f7e44ce36dfa90863fa911b55b0a6224
tjmellema <tmellema@umich.edu>
2026-02-18 11:55:19 -0500

    DOES NOT COMPILE - Limiter function and tests

--- src/limiters.hpp
+++ src/limiters.hpp
@@ -6,9 +6,27 @@
 //Requires:
 //Modifies:
 //Effects: Computes the gradient (no limiter) between a cell and its adjacent cells
-Tensor<1,2,double> Gradient(double l[], double A, Tensor<1,2,double> n[]);
+//  Written by Chat-GPT
+template<typename RealType>
+void ComputeGradients(const Triangulation<2,RealType>& tri,
+                      ElementData<2,RealType>& elem,
+                      std::vector<Tensor<1,2,RealType>>& grad_rho);
+
+//Requires: L0 is the cell gradient [1x2] for all 4 states
+//          u0 = cell state
+//          r = vectors to the cell corners --> computed by ComputeVertexVectors
+//Modifies:
+//Effects: Computes the scalar limiter gradient via Barth Jespenen
+Limiter_BJ(const Tensor<1,4,Tensor<1,2,double>>& L0,
+           const Tensor<1,4,double>& u0,
+           const std::array<Tensor<1,2,double>,3>& r);
 
 //Requires:
 //Modifies:
-//Effects: Computes a limited gradient based on Barth Jespenen
-Tensor<1,2,double> Limiter_BJ();
+//Effects: Computes the vertex vectors of a specific node
+//  Written by Chat-GPT
+template<typename RealType>
+std::array<Tensor<1,2,RealType>,3>
+ComputeVertexVectors(unsigned int elem_id,
+                     const MeshData& data,
+                     const ElementData<2,RealType>& elem);
--- tests/TestLimiter.cpp
+++ tests/TestLimiter.cpp
@@ -1,6 +1,50 @@
 #include <gtest/gtest.h>
+#include "limiter.hpp"
 
-TEST(test_suite, test)
+//test to ensure a zero gradient returns the constant state
+TEST(Gradient, ZeroGradient)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the gradient computes an arbitrary correct output
+TEST(Gradient, CorrectOutput)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the gradient computes a correct gradient on the periodic boundaries
+TEST(Gradient, PeriodicBoundary)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the gradient computes a correct gradient on a wall boundary
+TEST(Gradient, WallBoundary)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the gradient computes a correct gradient on a inflow boundary
+TEST(Gradient, WallBoundary)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the limited gradient computes an arbritrary correct output
+TEST(Limiter_BJ, CorrectOutput)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the limited gradient equates the 1st order when alpha = {0,0,0,0}
+TEST(Limiter_BJ, ZeroGradient)
+{
+  EXPECT_TRUE(true);
+}
+
+//Test to ensure the limited gradient equates the inputed gradient when alpha = {1,1,1,1}
+TEST(Limiter_BJ, NoLimiting)
 {
   EXPECT_TRUE(true);
 }
