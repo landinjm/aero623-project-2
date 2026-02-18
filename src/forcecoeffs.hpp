c4bec67f3af638af8de9bb3a349a11246b7a2ca3
LukeRandhawa <cadent_sugar@comcast.net>
2026-02-18 17:02:43 -0500

    should work with tensors?

--- src/forcecoeffs.hpp
+++ src/forcecoeffs.hpp
@@ -2,14 +2,15 @@
 
 #include <cmath>
 #include <vector>
-//#include <tensor.hpp>
+#include <tensor.hpp>
 
 class CalcForceCoeffs
 {
 public:
-    /* calcCpTensor(const Tensor<1, 2 + dim, RealType>& interior_state,
-                const Tensor<1, dim, RealType>& normal)
-        static_assert(dim == 2, "Only 2D is supported");
+    double calcCp(const Tensor<1, 2 + dim, RealType>& interior_state,
+                const Tensor<1, dim, RealType>& normal){
+        // Define useful variables
+        double gamma = 1.4;
 
         // Grab values
         const auto rho = interior_state[0];
@@ -18,41 +19,45 @@ public:
         const auto energy = interior_state[3];
 
         const auto n_x = normal[0];
-        const auto n_y = normal[1];) */
-
-    double calcCp(double rho, double momentum_x, double momentum_y, double energy){
-        // Define useful variables
-        double gamma = 1.4;
+        const auto n_y = normal[1];
 
         // Calculate useful variables
         double p0 = 1 / gamma; // rho0*a0^2/gamma but rho0 and a0 are 1 b/c nondimensional
         double pout = 0.7 * p0;
         double Mout2 = 2.0 / (gamma - 1.0) * (std::pow(p0 / pout, (gamma-1.0) / gamma) - 1);
         double qout = 0.5 * gamma * pout * Mout2;
-        double q = std::sqrt(momentum_x * momentum_x + momentum_y * momentum_y) / rho;
-        double p = (gamma - 1.0) * (energy - 0.5 * rho * q * q);
+        const auto q2 = (momentum_x * momentum_x + momentum_y * momentum_y) / rho;
+        const auto p = (Parameters<RealType>::gamma - 1.0) * (energy - 0.5 * rho * q2);
 
         // Calculate pressure coefficient
-        double cp = (p - pout) / qout;
+        const auto cp = (p - Parameters<RealType>::pout) / Parameters<RealType>::qout;
 
         // Return pressure coefficient
         return cp;
     }
 
     // also make this work with tensors
-    std::vector<double> calcForceCoeffs(double density, double momentum_x, double momentum_y, double energy,
-        double n_x, double n_y){
+    std::vector<double> calcForceCoeffs(const Tensor<1, 2 + dim, RealType>& interior_state,
+                const Tensor<1, dim, RealType>& normal){
         // Define useful variables
         double c = 18.804; // should be in mm, but I think x & y are also in mm
+        double c_x = 0.0;
+        double c_y = 0.0;
 
         // Get pressure coefficient
-        double cp = calcCp(density, momentum_x, momentum_y, energy);
+        const auto cp = calcCp(interior_state, normal);
+
+        // Get normal components
+        const auto n_x = normal[0];
+        const auto n_y = normal[1];
 
         // Calculate force coefficients
-        double cx = cp*n_x/c; // normal x component
-        double cy = cp*n_y/c; // normal y component
+        for (unsigned int i = 0; i < dim; ++i) {
+            c_x += cp * n_x / Parameters<RealType>::c;
+            c_y += cp * n_y / Parameters<RealType>::c;
+        }
 
         // Return force coefficients
-        return {cx, cy};
+        return {c_x, c_y};
     }
 };
