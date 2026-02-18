42942eb2ee8a79b6fe769757ff74c0bb3cb4e449
LukeRandhawa <cadent_sugar@comcast.net>
2026-02-18 17:02:43 -0500

    should work with tensors?

--- src/record.hpp
+++ src/record.hpp
@@ -3,22 +3,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-//#include <tensor.hpp>
+#include <vector>
+#include <tensor.hpp>
 
 class Recorder
 {
 public:
-    void recordHist(double data, std::string grid, std::string order, std::string type, std::string value){
-        // Open and append a text file
-        std::ofstream file((grid+"."+order+"."+type+"."+value+".txt"), std::ios::app);
-
-        // Write to the file
-        file << data << std::endl;
-
-        // Close the file
-        file.close();
-    }
-
     void cleanFile(std::string grid, std::string order, std::string type, std::string value){
         // Create/clear and open a text file
         std::ofstream file((grid+"."+order+"."+type+"."+value+".txt"));
@@ -27,24 +17,48 @@ public:
         file.close();
     }
 
-    // make this use tensor as well
-    void recordData(double density, double momentum_x, double momentum_y, double energy,
-                std::string grid, std::string order, std::string type, std::string value){
-        /*// Grab values
-        const auto rho = interior_state[0];
-        const auto momentum_x = interior_state[1];
-        const auto momentum_y = interior_state[2];
-        const auto energy = interior_state[3];*/
-        
+    void recordHist(std::vector<double> data, std::string grid, std::string order, std::string type, std::string value){
         // Open and append a text file
         std::ofstream file((grid+"."+order+"."+type+"."+value+".txt"), std::ios::app);
 
         // Write to the file
-        // for loop over each element
-        file << density << " " << momentum_x << " " << momentum_y << " " << energy << std::endl;
+        for (const auto& d : data) {
+            file << d << "\n";
+        }
+        file << std::endl;
 
         // Close the file
         file.close();
+    }
+
+    void recordData(ElementData<dim, RealType>& element_state,
+                std::string grid, std::string order, std::string type){
+        // Grab values
+        const auto rho = element_state[0];
+        const auto momentum_x = element_state[1];
+        const auto momentum_y = element_state[2];
+        const auto energy = element_state[3];
+        
+        // Open and append a text file
+        std::ofstream file((grid+"."+order+"."+type+".data.txt"), std::ios::app);
+
+        // Write to the file
+        for (unsigned int i = 0; i < element_state.size(); ++i) {
+            file << rho[i] << " " << momentum_x[i] << " " << momentum_y[i] << " " << energy[i] << "\n";
+        }
+        file << std::endl;
+
+        // Close the file
+        file.close();
+    }
+
+    void recordSimulation(ElementData<dim, RealType>& element_state,
+            std::vector<double> residual, std::vector<double> c_x, std::vector<double> c_y,
+            std::string grid, std::string order, std::string type){
+        recordData(element_state, grid, order, type);
+        recordHist(residual, grid, order, type, "residual");
+        recordHist(c_x, grid, order, type, "cx");
+        recordHist(c_y, grid, order, type, "cy");
     }
 };
 
