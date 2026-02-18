8ff3d5a6b2601f8f371b54d6fe94665e9d2a9ed3
landinjm <landinjm@umich.edu>
2026-02-18 18:17:25 -0500

    some fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Eigen/Dense>
 #include <iostream>
 #include <read_gri.hpp>
+#include <record.hpp>
 #include <solver.hpp>
 #include <timer.hpp>
 
@@ -19,10 +20,13 @@ main(int argc, char** argv)
   Triangulation<2, double> tria(reader.get_data());
   Timer::instance().end_section("create triangulation");
 
+  // Recorder and solver instances
+  Solver<2, 1, double> solver;
+  Recorder<2, double> recorder;
+
   // Free stream test
   unsigned int n_iterations = 1000;
   std::vector<double> residual_history(n_iterations, 0);
-  Solver<2, 1, double> solver;
 
   Timer::instance().begin_section("freestream - roe flux");
   std::cout << "\nFreestream Test - Roe Flux" << std::endl;
@@ -40,6 +44,7 @@ main(int argc, char** argv)
   }
   std::cout << "  Min/Max Residual " << min(residual_history) << "/"
             << max(residual_history) << std::endl;
+  recorder.recordHist(residual_history, "freestream", "1st", "roe", "residual");
   zero(residual_history);
   Timer::instance().end_section("freestream - roe flux");
 
@@ -59,21 +64,56 @@ main(int argc, char** argv)
   }
   std::cout << "  Min/Max Residual " << min(residual_history) << "/"
             << max(residual_history) << std::endl;
+  recorder.recordHist(
+    residual_history, "freestream", "1st", "HLLE", "residual");
   zero(residual_history);
   Timer::instance().end_section("freestream - HLLE flux");
 
   // Steady state
+  // TODO: Tolerance cutoff?
+  n_iterations = 10000;
+  residual_history.resize(n_iterations);
+
+  Timer::instance().begin_section("steadystate - roe flux");
+  std::cout << "\nSteadystate - Roe Flux" << std::endl;
   solver.set_free_stream_initial_state(tria.get_elements());
-  for (unsigned int i = 0; i < 10000; ++i) {
+  for (unsigned int i = 0; i < n_iterations; ++i) {
+    solver.compute_residual(tria.get_interior_faces(),
+                            tria.get_boundary_faces(),
+                            tria.get_periodic_faces(),
+                            tria.get_elements(),
+                            &flux_roe);
+    solver.compute_update_with_local_timestepping(tria.get_elements());
+    residual_history[i] = tria.get_elements().max_residual();
+  }
+  std::cout << "  Min/Max Residual " << min(residual_history) << "/"
+            << max(residual_history) << std::endl;
+  recorder.recordHist(
+    residual_history, "steadystate", "1st", "roe", "residual");
+  recorder.recordData(tria.get_elements(), "steadystate", "1st", "roe");
+  zero(residual_history);
+  Timer::instance().end_section("steadystate - roe flux");
+
+  Timer::instance().begin_section("steadystate - HLLE flux");
+  std::cout << "\nSteadystate - HLLE Flux" << std::endl;
+  solver.set_free_stream_initial_state(tria.get_elements());
+  for (unsigned int i = 0; i < n_iterations; ++i) {
     solver.compute_residual(tria.get_interior_faces(),
                             tria.get_boundary_faces(),
                             tria.get_periodic_faces(),
                             tria.get_elements(),
                             &Flux_HLLE);
     solver.compute_update_with_local_timestepping(tria.get_elements());
-    std::cout << "Residual: " << tria.get_elements().max_residual()
-              << std::endl;
+    residual_history[i] = tria.get_elements().max_residual();
   }
+  std::cout << "  Min/Max Residual " << min(residual_history) << "/"
+            << max(residual_history) << std::endl;
+  recorder.recordHist(
+    residual_history, "steadystate", "1st", "HLLE", "residual");
+  recorder.recordData(tria.get_elements(), "steadystate", "1st", "HLLE");
+  zero(residual_history);
+  Timer::instance().end_section("steadystate - HLLE flux");
+
   Timer::instance().summary();
 
   return 0;
