fa0bd3436dca36b86d87682e2200aba54015f895
landinjm <landinjm@umich.edu>
2026-03-09 18:04:27 -0400

    data out

--- main.cpp
+++ main.cpp
@@ -14,8 +14,17 @@ main(int argc, char* argv[])
     tria.generate_hyper_cube();
     tria.refine_global(4);
 
+    Vector<double, HostMemSpace> solution(tria.n_cells());
+
     DataOut<2> data_out;
-    data_out.write(tria, "test.vtu");
+    data_out.clear();
+    data_out.attach_triangulation(tria);
+    data_out.set_time(0.0);
+    data_out.set_cycle(0);
+
+    data_out.add_data_vector(solution, "solution");
+
+    data_out.write_vtu("test.vtu");
   }
   Kokkos::finalize();
 
