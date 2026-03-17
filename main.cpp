44a660379b88cf515ee86910e711eaaccae3a004
landinjm <landinjm@umich.edu>
2026-03-17 15:09:07 -0400

    dof handler and dg output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <Kokkos_Core.hpp>
 #include <config.hpp>
 #include <data_out.hpp>
+#include <dof_handler.hpp>
+#include <fe.hpp>
 #include <read_gri.hpp>
 #include <timer.hpp>
 #include <triangulation.hpp>
 #include <vector.hpp>
+
+static constexpr unsigned int problem_degree = 1;
 
 int
 main(int argc, char* argv[])
@@ -13,15 +17,25 @@ main(int argc, char* argv[])
   {
     GriReader<2> gri;
     Triangulation<2> tria;
+    FE_DGQLegendre<2, double> fe(problem_degree);
+    QGaussSimplex<2, double> quad(problem_degree + 1);
 
-    gri.read_gri("../grids/coarse_local_refinement_2.gri");
+    gri.read_gri("../grids/coarse_local_refinement_1.gri");
     gri.transfer_to_triangulation(tria);
 
-    Vector<double, HostMemSpace> solution(tria.n_cells());
+    DoFHandler<2, double> dof_handler(tria, fe);
+
+    std::cout << "Number of cells: " << tria.n_cells() << "\n";
+    std::cout << "Number of DoFs: " << dof_handler.n_dofs() << "\n";
+
+    Vector<double, HostMemSpace> solution(dof_handler.n_dofs());
+
+    solution[0] = 1.0;
+    solution[1] = 0.5;
 
     DataOut<2> data_out;
     data_out.clear();
-    data_out.attach_triangulation(tria);
+    data_out.attach_dof_handler(dof_handler);
     data_out.set_time(0.0);
     data_out.set_cycle(0);
 
--- src/data_out.hpp
+++ src/data_out.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Kokkos_Core.hpp>
+#include <dof_handler.hpp>
 #include <fstream>
 #include <stdexcept>
 #include <string>
@@ -13,7 +15,11 @@ class DataOut
 public:
   using Topo = SimplexTopology<dim>;
 
-  void attach_triangulation(const Triangulation<dim>& tria) { tria_ = &tria; }
+  void attach_dof_handler(const DoFHandler<dim, double>& dh)
+  {
+    dh_ = &dh;
+    tria_ = &dh.tria();
+  }
 
   void set_time(double t)
   {
@@ -24,8 +30,9 @@ public:
 
   void clear()
   {
-    data_entries_.clear();
+    entries_.clear();
     tria_ = nullptr;
+    dh_ = nullptr;
     has_time_ = false;
     time_ = 0.0;
     cycle_ = 0;
@@ -35,21 +42,33 @@ public:
   void add_data_vector(const Vector<RealType, HostMemSpace>& vec,
                        const std::string& name)
   {
-    data_entries_.emplace_back(name, vec);
+    ASSERT(dh_ != nullptr,
+           "attach_dof_handler must be called before add_dof_data");
+    ASSERT(vec.size() == dh_->n_dofs(), "Vector length must match n_dofs");
+    entries_.emplace_back(name, vec);
   }
 
   void write_vtu(const std::string& filename) const
   {
-    std::ofstream out(filename);
+    ASSERT(dh_ != nullptr, "No DoFHandler attached");
+    ASSERT(tria_ != nullptr, "No Triangulation attached");
+    ASSERT(!entries_.empty(), "No data vectors added");
 
-    const uint32_t n_pts = tria_->n_vertices();
-    const uint32_t n_cells = tria_->n_active_cells();
+    std::ofstream out(filename);
+    ASSERT(out.is_open(), "Could not open output file");
+
+    const uint32_t ndpc = dh_->n_dofs_per_cell();
+    const uint32_t n_cells = dh_->n_cells();
+    const uint32_t n_points = n_cells * ndpc;
 
     write_header(out);
-    write_piece_open(out, n_pts, n_cells);
-    write_points(out);
-    write_cells(out);
-    write_cell_data(out);
+    out << "    <Piece NumberOfPoints=\"" << n_points << "\" NumberOfCells=\""
+        << n_cells << "\">\n";
+
+    write_dg_points(out, ndpc);
+    write_dg_cells(out, ndpc, n_cells);
+    write_dg_point_data(out, ndpc);
+
     write_piece_close(out);
     write_footer(out);
   }
@@ -62,29 +81,145 @@ private:
   };
 
   const Triangulation<dim>* tria_ = nullptr;
-  std::vector<DataEntry> data_entries_;
+  const DoFHandler<dim, double>* dh_ = nullptr;
+
+  std::vector<DataEntry> entries_;
   double time_ = 0.0;
   unsigned int cycle_ = 0;
   bool has_time_ = false;
 
-  static constexpr uint8_t vtk_cell_type()
+  void write_dg_points(std::ofstream& out, uint32_t ndpc) const
   {
-    if constexpr (dim == 1) {
+    const auto& fe = dh_->fe();
+
+    out << "      <Points>\n"
+        << "        <DataArray type=\"Float64\""
+        << " NumberOfComponents=\"3\" format=\"ascii\">\n";
+
+    std::vector<uint32_t> local_dof_indices;
+    for (auto cell : dh_->active_cell_range()) {
+      cell.get_dof_indices(local_dof_indices);
+      auto jac = cell_jacobian(cell);
+
+      for (uint32_t i = 0; i < ndpc; ++i) {
+        auto x_phys = map_to_physical(cell, jac, fe.node(i));
+        for (unsigned int d = 0; d < dim; ++d)
+          out << x_phys(d) << " ";
+        for (unsigned int d = dim; d < 3; ++d)
+          out << "0.0 ";
+        out << "\n";
+      }
+    }
+
+    out << "        </DataArray>\n"
+        << "      </Points>\n";
+  }
+
+  void write_dg_cells(std::ofstream& out, uint32_t ndpc, uint32_t n_cells) const
+  {
+    out << "      <Cells>\n";
+
+    out << "        <DataArray type=\"UInt32\" Name=\"connectivity\""
+        << " format=\"ascii\">\n";
+    for (uint32_t k = 0; k < n_cells; ++k) {
+      for (uint32_t i = 0; i < ndpc; ++i)
+        out << k * ndpc + i << " ";
+      out << "\n";
+    }
+    out << "        </DataArray>\n";
+
+    out << "        <DataArray type=\"UInt32\" Name=\"offsets\""
+        << " format=\"ascii\">\n";
+    for (uint32_t k = 0; k < n_cells; ++k)
+      out << (k + 1) * ndpc << " ";
+    out << "\n        </DataArray>\n";
+
+    out << "        <DataArray type=\"UInt8\" Name=\"types\""
+        << " format=\"ascii\">\n";
+    const int vtype = static_cast<int>(vtk_cell_type_for_dofs(ndpc));
+    for (uint32_t k = 0; k < n_cells; ++k)
+      out << vtype << " ";
+    out << "\n        </DataArray>\n";
+
+    out << "      </Cells>\n";
+  }
+
+  void write_dg_point_data(std::ofstream& out, uint32_t ndpc) const
+  {
+    out << "      <PointData>\n";
+
+    std::vector<uint32_t> local_dof_indices;
+    for (const auto& e : entries_) {
+      out << "        <DataArray type=\"Float64\" Name=\"" << e.name
+          << "\" format=\"ascii\">\n";
+      for (auto cell : dh_->active_cell_range()) {
+        cell.get_dof_indices(local_dof_indices);
+        for (uint32_t i = 0; i < ndpc; ++i)
+          out << e.data[local_dof_indices[i]] << " ";
+        out << "\n";
+      }
+      out << "        </DataArray>\n";
+    }
+
+    out << "      </PointData>\n";
+  }
+
+  template<typename CellAcc>
+  std::array<Tensor<1, dim, double>, dim> cell_jacobian(
+    const CellAcc& cell) const
+  {
+    std::array<Tensor<1, dim, double>, dim> jac;
+    auto x0 = cell.vertex(0);
+    for (unsigned int d = 0; d < dim; ++d) {
+      auto xd = cell.vertex(d + 1);
+      for (unsigned int d2 = 0; d2 < dim; ++d2)
+        jac[d](d2) = xd[d2] - x0[d2];
+    }
+    return jac;
+  }
+
+  template<typename CellAcc>
+  Tensor<1, dim, double> map_to_physical(
+    const CellAcc& cell,
+    const std::array<Tensor<1, dim, double>, dim>& jac,
+    const Tensor<1, dim, double>& x_ref) const
+  {
+    auto x0 = cell.vertex(0);
+    Tensor<1, dim, double> x_phys;
+    for (unsigned int d = 0; d < dim; ++d)
+      x_phys(d) = x0[d];
+    for (unsigned int d2 = 0; d2 < dim; ++d2)
+      for (unsigned int d = 0; d < dim; ++d)
+        x_phys(d) += jac[d2](d) * x_ref(d2);
+    return x_phys;
+  }
+
+  static uint8_t vtk_cell_type_for_dofs(uint32_t ndpc)
+  {
+    if constexpr (dim == 1)
       return 3;
-    }
     if constexpr (dim == 2) {
-      return 5;
+      switch (ndpc) {
+        case 3:
+          return 5; // VTK_TRIANGLE
+        case 6:
+          return 22; // VTK_QUADRATIC_TRIANGLE
+        case 10:
+          return 69; // VTK_LAGRANGE_TRIANGLE (VTK 9+)
+        default:
+          return 7; // VTK_POLYGON
+      }
     }
-    if constexpr (dim == 3) {
+    if constexpr (dim == 3)
       return 10;
-    }
+    return 7;
   }
 
   void write_header(std::ofstream& out) const
   {
-    out << "<?xml version=\"1.0\"?>\n";
-    out << "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" "
-        << "byte_order=\"LittleEndian\">\n"
+    out << "<?xml version=\"1.0\"?>\n"
+        << "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\""
+        << " byte_order=\"LittleEndian\">\n"
         << "  <UnstructuredGrid>\n";
     if (has_time_) {
       out << "    <FieldData>\n"
@@ -102,74 +237,5 @@ private:
         << "</VTKFile>\n";
   }
 
-  void write_piece_open(std::ofstream& out,
-                        uint32_t n_pts,
-                        uint32_t n_cells) const
-  {
-    out << "    <Piece NumberOfPoints=\"" << n_pts << "\" NumberOfCells=\""
-        << n_cells << "\">\n";
-  }
-
   void write_piece_close(std::ofstream& out) const { out << "    </Piece>\n"; }
-
-  void write_points(std::ofstream& out) const
-  {
-    out << "      <Points>\n"
-        << "        <DataArray type=\"Float64\""
-        << " NumberOfComponents=\"3\" format=\"ascii\">\n";
-    for (uint32_t i = 0; i < tria_->n_vertices(); ++i) {
-      for (int d = 0; d < dim; ++d) {
-        out << tria_->vertices(i, d) << " ";
-      }
-      for (int d = dim; d < 3; ++d) {
-        out << "0.0 ";
-      }
-      out << "\n";
-    }
-    out << "        </DataArray>\n"
-        << "      </Points>\n";
-  }
-
-  void write_cells(std::ofstream& out) const
-  {
-    out << "      <Cells>\n";
-    out << "        <DataArray type=\"UInt32\" Name=\"connectivity\" "
-           "format=\"ascii\">\n";
-    for (size_t i = 0; i < tria_->n_active_cells(); ++i) {
-      for (uint8_t v = 0; v < Topo::verts_per_cell; ++v) {
-        out << tria_->cell_verts(i, v) << " ";
-      }
-      out << "\n";
-    }
-    out << "        </DataArray>\n";
-    out << "        <DataArray type=\"UInt32\" Name=\"offsets\" "
-           "format=\"ascii\">\n";
-    uint32_t offset = 0;
-    for (size_t i = 0; i < tria_->n_active_cells(); ++i) {
-      offset += Topo::verts_per_cell;
-      out << offset << " ";
-    }
-    out << "\n        </DataArray>\n";
-    out << "        <DataArray type=\"UInt8\" Name=\"types\" "
-           "format=\"ascii\">\n";
-    for (size_t i = 0; i < tria_->n_active_cells(); ++i) {
-      out << static_cast<int>(vtk_cell_type()) << " ";
-    }
-    out << "\n        </DataArray>\n";
-    out << "      </Cells>\n";
-  }
-
-  void write_cell_data(std::ofstream& out) const
-  {
-    out << "      <CellData>\n";
-    for (auto& e : data_entries_) {
-      out << "        <DataArray type=\"Float64\" Name=\"" << e.name
-          << "\" format=\"ascii\">\n";
-      for (size_t i = 0; i < tria_->n_active_cells(); ++i) {
-        out << e.data[i] << " ";
-      }
-      out << "\n        </DataArray>\n";
-    }
-    out << "      </CellData>\n";
-  }
 };
