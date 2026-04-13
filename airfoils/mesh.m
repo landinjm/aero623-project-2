a08f0a36ae7f95448471c4d5f36f96c25e136682
matchuepi <131813086+matchuepi@users.noreply.github.com>
2026-04-13 17:43:53 -0400

    Cube (#103)
    * naca 2412 airfoils stl for 0deg and 3deg with 10% thickness of chord
    
    * 9512
    
    * cube.stl

---
+++ airfoils/mesh.m
@@ -0,0 +1,9 @@
+clear; clc; close all;
+
+gm = fegeometry("cube.stl");
+gm = generateMesh(gm,Hmax=0.1,GeometricOrder="linear");
+pdemesh(gm,FaceAlpha=0.5)
+
+% gm = generateMesh(gm,Hmax=0.3, ...
+%                   GeometricOrder="linear");
+% pdemesh(gm)
