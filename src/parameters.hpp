5f663e31ff9a45174a961d09e04e00996512006a
landinjm <landinjm@umich.edu>
2026-04-14 23:36:54 -0400

    okie

--- src/parameters.hpp
+++ src/parameters.hpp
@@ -75,7 +75,7 @@ struct Parameters
   /**
    * @brief Maximum CFL number to use when calculating optimal timestep.
    */
-  static constexpr RealType cfl_max = RealType{ 0.008 };
+  static constexpr RealType cfl_max = RealType{ 0.8 };
 
   /**
    * @brief Convergence tolerance.
