2ff1d47189d229da2edd6538a5870437a77e152b
thakurtr <66138046+thakurtr@users.noreply.github.com>
2026-04-20 14:37:25 -0400

    curved mesh for cube, and semi updated writegri (#122)

--- airfoils/Mesh3d.m
+++ airfoils/Mesh3d.m
@@ -1,8 +1,13 @@
 function Mesh3d(filename,hmax)
     % will read an stl file and write into a gri
     model = femodel(Geometry=filename+".stl");
-    mesh = generateMesh(model,Hmax=hmax,Hmin=0.001,Hgrad=1.2,GeometricOrder="linear");
+    mesh = generateMesh(model,Hmax=hmax,Hmin=0.001,Hgrad=1.2,GeometricOrder="quadratic");
     Mesh = mesh.Geometry.Mesh;
+    disp('Size of mesh.Nodes:')
+    disp(size(Mesh.Nodes))
+
+    disp('Size of mesh.Elements:')
+    disp(size(Mesh.Elements))
     pdemesh(mesh,FaceAlpha=0.5,ElementLabels="off",NodeLabels="off")
     write_gri_3d(filename+".gri",Mesh.Nodes',Mesh.Elements')
 end
